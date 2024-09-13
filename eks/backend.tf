81a37a785273e56fc1b630fe9c34756ace5a436e
vakabinto <113638813+vakabinto@users.noreply.github.com>
2024-09-13 13:33:50 -0700

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "eks-buck"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Lock-files"
     encrypt        = true
   }
 }
